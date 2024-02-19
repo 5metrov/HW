669710a011f9edddb59b9847d0a878413b830638
Danissimo <160459367+5metrov@users.noreply.github.com>
2024-02-19 09:09:23 +0300

    just hw program

---
+++ 19,02,24cpp_version_control.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+void osn0() {
+    int unum = 0;
+    do {
+        cout << "Hello World! \nВывести сообщение ещё раз? 1 - да; 2 - нет;\n";
+        cin >> unum;
+        switch (unum) {
+        case 1: continue; break;
+        case 2: break; break;
+        }
+    } while (unum != 2);
+}
+
+int main(){
+    setlocale(LC_ALL, "ru");
+    osn0();
+    return 0;
+}
+
